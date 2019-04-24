1056ba76c93bdcbe0f69efa6c653915b1cb03fbe
joshua-miles <joshua@christetulit.org>
2019-04-24 14:34:31 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <List />
   );
+}
+const taskList = [{id:1, content:'eat all the food!!!!', done:false},
+{id:2, content:'cardio', done:false},
+{id:3, content:'die at like 2', done:true},
+{id:4, content:'wake up', done:false}]
+
+class List extends Component {
+  
+  state = {
+    listOfTasks: taskList
+  }
+
+  render() {
+    return <ul>{this.state.listOfTasks.map(task => < Task id={task.id} content={task.content} isDone={task.done} toCheck={this.toCheck}/>)}</ul>
+  }
+  toCheck = (id) => {
+    console.log(id)
+    this.setState({
+      listOfTasks: this.state.listOfTasks.map(task => {
+        if(task.id == id){
+          return {
+            ...task, done: !task.done
+          }
+        } else {
+          return task
+        }
+      })
+    })
+  }
+
+}
+
+class Task extends Component {
+
+  render() {
+    return <li><input type= 'checkbox' checked = {this.props.isDone} onChange = {() => this.props.toCheck(this.props.id)}></input>  {this.props.content}</li>
+  }
+
 }
 
 export default App;
